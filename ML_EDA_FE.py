f28cff7f3caaf49079bed5d3f2fca21611d950e0
lakshmi-chandana-namburu <104778781+lakshmi-chandana-namburu@users.noreply.github.com>
2022-12-30 21:29:38 +0530

    did EDA and FE

---
+++ ML_EDA_FE.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import os
+from sklearn.impute import SimpleImputer # used to fill missing data
+from sklearn import tree
+from sklearn import model_selection
+tt=pd.read_csv(r"D:\20_594\blackbucks_level2\titanic_dataset\train.csv")
+ttest=pd.read_csv(r"D:\20_594\blackbucks_level2\titanic_dataset\test.csv")
+ttest.Survived=None
+t=pd.concat([tt,ttest])
+t.shape
+t.info()
+def extract_title(name):
+    return name.split(',')[1].split('.')[0].strip()
+t['Title']=t['Name'].map(extract_title)
+mean_imputer=SimpleImputer()
+mean_imputer.fit(tt[['Age','Fare']])
+t[['Age','Fare']]=mean_imputer.transform(t[['Age','Fare']])
+def convert_age(age):
+    if(age>=0 and age<=10):
+        return 'Child'
+    elif(age <= 25): 
+        return 'Young'
+    elif(age <= 50): 
+        return 'Middle'
+    else: 
+        return 'Old'
+#Convert numerical Age column to categorical Age_Cat column
+t['Age_Cat'] = t['Age'].map(convert_age)
+t['FamilySize'] = t['SibSp'] +  t['Parch'] + 1
+def convert_familysize(size):
+    if(size == 1): 
+        return 'Single'
+    elif(size <=3): 
+        return 'Small'
+    elif(size <= 6): 
+        return 'Medium'
+    else: 
+        return 'Large'
+t['FamilySize_Cat'] = t['FamilySize'].map(convert_familysize)
+titanic1 = pd.get_dummies(t, columns=['Sex','Pclass','Embarked', 'Age_Cat', 'Title', 'FamilySize_Cat'])
+titanic2 = titanic1.drop(['PassengerId','Name','Age','Ticket','Cabin','Survived'], axis=1, inplace=False)
+X_train = titanic2[0:891] #0 t0 891 records
+X_train.shape
+X_train.info()
+y_train = tt['Survived']
+tree_estimator = tree.DecisionTreeClassifier()
+dt_grid = {'max_depth':list(range(10,30)), 'min_samples_split':list(range(2,8)), 'criterion':['gini','entropy']}
+param_grid = model_selection.GridSearchCV(tree_estimator, dt_grid, cv=9) #Evolution of tee
+param_grid.fit(X_train, y_train) 
+X_test = titanic2[tt.shape[0]:] #shape[0]: means 0 index to n index. Not specifying end index is nothing but till nth index
+X_test.shape
+X_test.info()
+ttest['Survived'] = param_grid.predict(X_test)
+tt.to_csv(r"D:\20_594\blackbucks_level2\Attempt5.csv", columns=['PassengerId','Survived'],index=False)
